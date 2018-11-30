48e2d224055f9a7a17f69fe7db189a23ceabb69c
Your Name <you@example.com>
2018-11-30 15:18:32 +0800

    mysql run config uuid

--- 5.7/docker-entrypoint.sh
+++ 5.7/docker-entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 # do something before mysql start
-
-# create uuid
-psd="/proc/sys/kernel/random/uuid"
-str=$(cat $psd)
-uuid="server-uuid="${str}
-echo "[auto]" > /var/lib/mysql/auto.cnf
-echo $uuid >> /var/lib/mysql/auto.cnf
-
 ## read env and create mysql config file
 /usr/local/bin/env2file create --format mysql --path /etc/mysql/conf.d/custom.cnf
 # start mysql
