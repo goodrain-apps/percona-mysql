1f5898efa2e5d5333a7de9a34dd70b04fcb13a46
Your Name <you@example.com>
2018-12-03 11:28:26 +0800

    create server_id

--- 5.7/docker-entrypoint.sh
+++ 5.7/docker-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # do something before mysql start
+
+# create server_id
+server_id=${HOSTNAME#*-}
+MYSQLC_MYSQLD_server_id=`expr server_id + 1`
+export MYSQLC_MYSQLD_SERVER_ID
+
 ## read env and create mysql config file
 /usr/local/bin/env2file create --format mysql --path /etc/mysql/conf.d/custom.cnf
 # start mysql
