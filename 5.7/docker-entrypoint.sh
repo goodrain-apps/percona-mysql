98e4936fcc38413023fe9628eebfad998f5be8f7
Your Name <you@example.com>
2018-12-04 16:34:31 +0800

    server_id config

--- 5.7/docker-entrypoint.sh
+++ 5.7/docker-entrypoint.sh
@@ -2,9 +2,8 @@
 # do something before mysql start
 
 # create server_id
-#HOSTNAME=gr31895c-0
 server_id=${HOSTNAME#*-}
-MYSQLC_MYSQLD_SERVER_ID=`expr $server_id + 2`
+MYSQLC_MYSQLD_SERVER_ID=`expr $server_id + ${STEP_NUM}`
 export MYSQLC_MYSQLD_SERVER_ID
 
 ## read env and create mysql config file
