631e1501384466b04863667e9a405ba68c6f7662
guox <guox@goodrain.com>
2019-06-24 17:50:11 +0800

    Update docker-entrypoint.sh

--- 5.7/docker-entrypoint.sh
+++ 5.7/docker-entrypoint.sh
@@ -2,9 +2,9 @@
 # do something before mysql start
 
 # create server_id
-server_id=${HOSTNAME#*-}
-MYSQLC_MYSQLD_SERVER_ID=`expr $server_id + ${STEP_NUM}`
-export MYSQLC_MYSQLD_SERVER_ID
+# server_id=${HOSTNAME#*-}
+# MYSQLC_MYSQLD_SERVER_ID=`expr $server_id + ${STEP_NUM}`
+# export MYSQLC_MYSQLD_SERVER_ID
 
 ## read env and create mysql config file
 /usr/local/bin/env2file create --format mysql --path /etc/mysql/conf.d/custom.cnf
