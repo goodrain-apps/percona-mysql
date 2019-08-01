85d13babf50753b4261561c340ff25dc506b8cf3
guox <guox@goodrain.com>
2019-08-01 23:18:53 +0800

    Update init-cluster.sh

--- 5.7/sql/init-cluster.sh
+++ 5.7/sql/init-cluster.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+[[ $DEBUG ]] && set -x 
 while true 
-do 
-mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -D${MYSQL_DATABASE} -P${MYSQL_PORT} -e "show tables;" > /dev/null
-if [ $? == 0 ];then
-    mysql -uroot -p${MYSQL_ROOT_PASSWORD} -P${MYSQLC_MYSQLD_PORT} -e "change master to master_host='127.0.0.1', master_port=3306, master_user='root', master_password='${MYSQL_ROOT_PASSWORD}';start slave;"
+do
+# start slave 
+mysql -uroot -p${MYSQL_ROOT_PASSWORD} -P${MYSQLC_MYSQLD_PORT} -e "change master to master_host='127.0.0.1', master_port=3306, master_user='root', master_password='${MYSQL_ROOT_PASSWORD}';start slave;"
+# check it
+mysql -uroot -p${MYSQL_ROOT_PASSWORD} -P${MYSQLC_MYSQLD_PORT} -e "show slave status\G;" | grep "Slave_IO_Running: Yes"
+if [ $? == 0 ];then    
     break
 fi
 echo "Waiting master DB init··· retry in 3 seconds" 
