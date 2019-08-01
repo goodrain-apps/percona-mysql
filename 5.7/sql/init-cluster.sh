46021360a5309cc3a5d8024e0d349ea562a1fbaf
guox <guox@goodrain.com>
2019-08-01 14:27:00 +0800

    [UPDATE] add init method

---
+++ 5.7/sql/init-cluster.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mysql -uroot -p${MYSQL_ROOT_PASSWORD} -P${MYSQLC_MYSQLD_PORT} -e "change master to master_host='127.0.0.1', master_port=3306, master_user='root', master_password="${MYSQL_ROOT_PASSWORD}";"
