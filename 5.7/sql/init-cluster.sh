f65b0b08c8de7220a78827f070333dd028efdeb1
guox <guox@goodrain.com>
2019-08-01 15:08:18 +0800

    fix

--- 5.7/sql/init-cluster.sh
+++ 5.7/sql/init-cluster.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mysql -uroot -p${MYSQL_ROOT_PASSWORD} -P${MYSQLC_MYSQLD_PORT} -e "change master to master_host='127.0.0.1', master_port=3306, master_user='root', master_password="${MYSQL_ROOT_PASSWORD}";start slave;"
+mysql -uroot -p${MYSQL_ROOT_PASSWORD} -P${MYSQLC_MYSQLD_PORT} -e "change master to master_host='127.0.0.1', master_port=3306, master_user='root', master_password='${MYSQL_ROOT_PASSWORD}';start slave;"
