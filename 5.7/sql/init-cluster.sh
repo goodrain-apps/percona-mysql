df383efe31bb326dd013338b4030f4edba0be740
guox <guox@goodrain.com>
2019-08-01 22:20:50 +0800

    Update init-cluster.sh

--- 5.7/sql/init-cluster.sh
+++ 5.7/sql/init-cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true 
 do 
-mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -d${MYSQL_DATABASE} -P${MYSQL_PORT} -e "show tables;" > /dev/null 2&>1
+mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -D${MYSQL_DATABASE} -P${MYSQL_PORT} -e "show tables;" > /dev/null
 if [ $? == 0 ];then
     mysql -uroot -p${MYSQL_ROOT_PASSWORD} -P${MYSQLC_MYSQLD_PORT} -e "change master to master_host='127.0.0.1', master_port=3306, master_user='root', master_password='${MYSQL_ROOT_PASSWORD}';start slave;"
     break
